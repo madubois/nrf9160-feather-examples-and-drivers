2488623c1e983bdd83fc3a99b287643fd6184860
Jared Wolff <hello@jaredwolff.com>
2024-08-27 09:27:06 -0400

    Updating board defintion from v2.7.x branch
    Syncing board defintion from upstream
    
    Signed-off-by Jared Wolff <hello@jaredwolff.com>

--- boards/arm/circuitdojo_feather_nrf9161/startup.c
+++ boards/arm/circuitdojo_feather_nrf9161/startup.c
@@ -1,11 +1,8 @@
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/mfd/npm1300.h>
 #include <zephyr/logging/log.h>
-
+#include <zephyr/drivers/mfd/npm1300.h>
 LOG_MODULE_REGISTER(app_startup);
-
-#if IS_ENABLED(CONFIG_MFD_NPM1300)
 
 #define SYSREG_VBUSIN_BASE 0x02
 
@@ -13,7 +10,7 @@ LOG_MODULE_REGISTER(app_startup);
 #define SYSREG_VBUSINILIM0 0x01
 #define SYSREG_VBUSINILIM_1000MA 0x0a
 
-static int sample_setup(void)
+static int sysreg_setup(void)
 {
 
     /* Get pmic */
@@ -24,8 +21,27 @@ static int sample_setup(void)
         return -ENODEV;
     }
 
-    /* Write to MFD to set SYSREG current to 1A */
-    int ret = mfd_npm1300_reg_write(pmic, SYSREG_VBUSIN_BASE, SYSREG_VBUSINILIM0, 0);
+    uint8_t data = 0;
+    int ret = mfd_npm1300_reg_read_burst(pmic, SYSREG_VBUSIN_BASE, SYSREG_VBUSINILIM0, &data, 1);
+    if (ret < 0)
+    {
+        printk("Failed to read VBUSINLIM. Err: %d", ret);
+        return ret;
+    }
+    else
+    {
+        if (data == 0)
+        {
+            data = 5;
+
+            printk("*** Vsys Current Limit: %d mA ***\n", data * 100);
+
+            return 0;
+        }
+    }
+
+    /* Write to MFD to set SYSREG current to 500mA */
+    ret = mfd_npm1300_reg_write(pmic, SYSREG_VBUSIN_BASE, SYSREG_VBUSINILIM0, 0);
     if (ret < 0)
     {
         printk("Failed to set VBUSINLIM. Err: %d\n", ret);
@@ -40,25 +56,10 @@ static int sample_setup(void)
         return ret;
     }
 
-    uint8_t data = 0;
-    ret = mfd_npm1300_reg_read_burst(pmic, SYSREG_VBUSIN_BASE, SYSREG_VBUSINILIM0, &data, 1);
-    if (ret < 0)
-    {
-        printk("Failed to read VBUSINLIM. Err: %d", ret);
-        return ret;
-    }
-    else
-    {
-        printk("Vsys Current Limit: %d mA\n", data * 100);
-    }
+    /* Delay boot for programmer */
+    k_sleep(K_SECONDS(2));
 
     return 0;
 }
-#else
-static int sample_setup(void)
-{
-    return 0;
-}
-#endif
 
-SYS_INIT(sample_setup, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+SYS_INIT(sysreg_setup, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
